df17e8fbede24a9759373d7552e541baac955d1b
Vedika K N <112706437+Vedikakodi@users.noreply.github.com>
2025-03-21 11:09:44 +0530

    Add files via upload

---
+++ DataStore.Abstraction/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataStore.Abstraction.Models
+{
+    public class User
+    {
+        public int Id { get; set; }  // Auto-incremented by SQL
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
+}
---
+++ DataStore.Abstraction/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataStore.Abstraction.Models;
+using System.Threading.Tasks;
+
+namespace DataStore.Abstraction.Repositories
+{
+    public interface IUserRepository
+    {
+        Task AddUser(User user);
+        Task<User> GetUserByEmail(string email);
+    }
+
+
+
+}
